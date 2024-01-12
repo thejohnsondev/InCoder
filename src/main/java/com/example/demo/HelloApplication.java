5982f8a65ecc85bd017aec2ec9b93a9c99bbe08e
Andrew <thejohnsondev@gmail.com>
2024-01-12 18:18:04 +0200

    copy images to main source

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -13,21 +13,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
-//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("assets/fxmls/mainWindow.fxml"));
-//        fxmlLoader.setRoot(new AnchorPane());
-//
-//        Parent root = fxmlLoader.load();
-//        primaryStage.setTitle("InCoder 2");
-//        primaryStage.getIcons().add(new Image("com/sample/assets/img/Icon.png"));
-//        primaryStage.setScene(new Scene(root, 1080, 800));
-//        primaryStage.setResizable(false);
-//        primaryStage.show();
-
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainWindow.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1080, 800);
         stage.setTitle("InCoder 2");
-//        stage.getIcons().add(new Image("com/sample/assets/img/Icon.png"));
         stage.setScene(scene);
         stage.setResizable(false);;
         stage.show();
